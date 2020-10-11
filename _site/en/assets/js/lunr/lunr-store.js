af1e24509c7492c1a32e4c14d929e3fb1a5aa350
kosehy@gmail.com <kosehy@gmail.com>
2020-10-12 01:44:05 +0900

    rename post folder

--- _site/en/assets/js/lunr/lunr-store.js
+++ _site/en/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "hello!",
+        "excerpt":"Hello   2020-10-11 18:15:03 +0900  ","categories": ["Jekyll"],
+        "tags": [],
+        "url": "kosehy.github.io/en/2020/10/2020-10-11-01/",
+        "teaser": "kosehy.github.io/assets/images/kosehy.jpg"
+      }]
