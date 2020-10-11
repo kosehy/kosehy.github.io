4bf4acbff530d6f0d8643b69076b22095d7b404c
kosehy@gmail.com <kosehy@gmail.com>
2020-10-12 01:55:26 +0900

    rename ko folder and rake publish

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "헬로!",
+        "excerpt":"안녕   2020-10-11 18:15:03 +0900  ","categories": ["Jekyll"],
+        "tags": [],
+        "url": "kosehy.github.io/2020/10/2020-10-11-01/",
+        "teaser": "kosehy.github.io/assets/images/kosehy.jpg"
+      }]
--- _site/jp/assets/js/lunr/lunr-store.js
+++ _site/jp/assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "こんにちわ!",
         "excerpt":"こんにちわ   2020-10-11 18:15:03 +0900  ","categories": ["Jekyll"],
         "tags": [],
-        "url": "kosehy.github.io/en/2020/10/2020-10-11-01/",
+        "url": "kosehy.github.io/2020/10/2020-10-11-01/",
         "teaser": "kosehy.github.io/assets/images/kosehy.jpg"
       }]
