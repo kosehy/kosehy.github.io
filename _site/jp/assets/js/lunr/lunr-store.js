d1c02fe4663912701af0eb2525940681bdcf34ce
kosehy@gmail.com <kosehy@gmail.com>
2020-10-13 00:52:29 +0900

    Add test00 post

--- _site/jp/assets/js/lunr/lunr-store.js
+++ _site/jp/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "kosehy.github.io/2020/10/2020-10-11-01/",
         "teaser": "kosehy.github.io/assets/images/kosehy.jpg"
+      },{
+        "title": "テスト00!",
+        "excerpt":"テスト00   2020-10-13 01:27:00 +0900  ","categories": ["Jekyll"],
+        "tags": [],
+        "url": "kosehy.github.io/2020/10/2020-10-13-01/",
+        "teaser": "kosehy.github.io/assets/images/kosehy.jpg"
       }]
